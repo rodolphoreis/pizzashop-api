d654c54f41815b41da5f8c2dfbe4b3217eb480a9
Rodolpho Reis <rodolphoreis@live.com>
2024-12-28 11:02:36 +0000

    feat: add optional in RESEND

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,14 @@
-import { z } from 'zod'
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+import { z } from "zod";
 
 const envSchema = z.object({
   API_BASE_URL: z.string().url(),
   AUTH_REDIRECT_URL: z.string().url(),
   DB_URL: z.string().url().min(1),
   JWT_SECRET_KEY: z.string().min(1),
-  RESEND_API_KEY: z.string().min(1),
-})
+  RESEND_API_KEY: z.string().min(1).optional(),
+});
 
-export const env = envSchema.parse(process.env)
+// eslint-disable-next-line prettier/prettier
+export const env = envSchema.parse(process.env);
